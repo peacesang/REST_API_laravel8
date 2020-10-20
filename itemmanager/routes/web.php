a03c7b479814df8b975ff2b08ae70071eebe2b28
peacesang <realfriend2u@gmail.com>
2020-10-20 18:09:06 +0545

    store and show data with api

--- itemmanager/routes/web.php
+++ itemmanager/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ItemsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('api/items', [ItemsController::class, 'index']);
+
+Route::resource('api/items', ItemsController::class);
