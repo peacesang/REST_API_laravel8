8b3c567fc9628a85eb4ae9b58c9a6a4491e7f2dd
peacesang <realfriend2u@gmail.com>
2020-10-20 18:27:29 +0545

    update and delete in api

--- itemmanager/app/Http/Controllers/ItemsController.php
+++ itemmanager/app/Http/Controllers/ItemsController.php
@@ -89,6 +89,21 @@ class ItemsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $validator=Validator::make($request->all(),[
+            'text'=>'required'
+        ]);
+        if($validator->fails()){
+            $response=array('response'=>$validator->messages(), 'success'=>false);
+            return $response;
+        } else{
+
+            $item=Item::find($id);
+            $item->text=$request->input('text');
+            $item->body=$request->input('body');
+            $item->save();
+
+            return response()->json($item);
+        }
     }
 
     /**
@@ -100,5 +115,12 @@ class ItemsController extends Controller
     public function destroy($id)
     {
         //
+        $item=Item::find($id);
+        $item->delete();
+
+        $response=array('response'=>'item deleted', 'success'=>true);
+            
+
+            return $response;
     }
 }
